e38aeb0f9bc2cb7e350d828cac9e6a7947e5f992
Kai Niebes <kai.niebes@outlook.com>
2021-12-08 07:56:10 +0100

    Update Day 06

--- aoc06.cr
+++ aoc06.cr
@@ -2,21 +2,21 @@ DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
 INPUT = File.read_lines("#{DAY}.txt").map(&.split(",").map(&.to_i)).first
 
 def solve(days)
-  fish = Hash(Int32, Int64).new { 0_i64 }
+  fish = Array.new(9, 0_i64)
   INPUT.each { |i| fish[i] += 1_i64 }
   days.times do |_|
-    newfish = Hash(Int32, Int64).new { 0_i64 }
-    fish.each do |k, v|
-      if k == 0
-        newfish[6] += v
-        newfish[8] += v
+    newfish = Array.new(9, 0_i64)
+    fish.each_with_index do |fishes, index|
+      if index == 0
+        newfish[6] += fishes
+        newfish[8] += fishes
       else
-        newfish[k - 1] += v
+        newfish[index - 1] += fishes
       end
     end
     fish = newfish
   end
-  fish.values.sum
+  fish.sum
 end
 
 def part1
